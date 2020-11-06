1e5c34929ffde260bcc6d0cc7c754db87fb4c82d
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-06 16:14:12 -0600

    created a local state and handleChange function in AddMovie component

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -3,6 +3,19 @@ import { connect } from 'react-redux';
 
 
 class AddMovie extends Component {
+    //create a local state
+    state = {
+        addingMovie: ''
+    }
+
+    handleChange = event => {
+        console.log('event happended')
+        this.setState({
+            addingMovie: {
+                addingMovie: event.target.value,
+            }
+        });
+    }
 
     render() {
       return (
@@ -11,6 +24,27 @@ class AddMovie extends Component {
           <input>Movie Title</input>
           <input>Poster Image</input>
           <p>Text Area</p>
+          <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick a genre:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="Adventure">Adventure</option>
+            <option value="Animated">Animated</option>
+            <option value="Biographical">Biographical</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Disaster">Disaster</option>
+            <option value="Drama">Drama</option>
+            <option value="Epic">Epic</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Musical">Musical</option>
+            <option value="Romantic">Romantic</option>
+            <option value="Science Fiction">Science Fiction</option>
+            <option value="Space-Opera">Space-Opera</option>
+            <option value="Superhero">Superhero</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
           <buttton>Cancel</buttton>
           <buttton>Save</buttton>
         </div>
