b0693c63020e49321f847c8f5ba5061a984abab4
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-08 15:13:25 -0600

    fixed the null error and undefined error

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const genres = (state = [], action) => {
     }
 }
 //reducer for details with switch
-const details = (state = {}, action) => {
+const details = (state = [], action) => {
     switch (action.type) {
         case 'SET_DETAILS':
             return action.payload;
